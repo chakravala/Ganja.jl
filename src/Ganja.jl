dbbe668468c29386c861db767d05d33fb049a378
Michael Reed <18372368+chakravala@users.noreply.github.com>
2023-01-18 05:49:11 -0500

    upgraded to Grassmann.jl v0.8

--- src/Ganja.jl
+++ src/Ganja.jl
@@ -24,7 +24,14 @@ loadexample(filename) = (w=loadganja();loadexample(w,filename);w)
 function __init__()
     @require Grassmann="4df31cd9-4c27-5bea-88d0-e6a7146666d8" begin
         export addelement!
-        addelement!(w,t) = js(w,JSString("addElement($(string(value(Grassmann.MultiVector(t)))))"))
+        addelement!(w,t) = js(w,JSString("addElement($(string(value(Grassmann.Multivector(t)))))"))
+    end
+    @require Reduce="93e0c654-6965-5f22-aba9-9c1ae6b3c259" begin
+        export multigraph!
+        function multigraph!(w,t::Expr)
+            t.head ≠ :-> && throw(error("$t is not an anonymous function"))
+            js(w,JSString(readexample("multi-graphs-head.js")*"$(t.args[1])=>$(Reduce.RExpr(t.args[2]))"*readexample("multi-graphs-plot.js")))
+        end
     end
 end
 
